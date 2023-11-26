f1442e443633abd086e10f34d4ba6b632ba0f696
= <=>
2023-11-26 13:49:59 +0400

    get new nft minted

--- src/blockchain/blockchain.service.ts
+++ src/blockchain/blockchain.service.ts
@@ -8,24 +8,38 @@ import * as moment from 'moment-timezone';
 @Injectable()
 export class BlockchainService {
   ownerWallet: string;
-  abi = ['event Transfer(address indexed from, address indexed to, uint amount)'];
-  provider;
+  ownerNftWallet: string;
+  nftAddress: string;
+  abiTransferEvent = ['event Transfer(address indexed from, address indexed to, uint amount)'];
+  abiNft = [
+    'function getTokenLevel(uint256) public view returns (uint256)',
+    'event Transfer(address indexed from, address indexed to, uint256 indexed tokenId)',
+  ];
+  providerAnkr;
+  providerBsc;
   constructor(
     private configService: ConfigService,
     private exchangeService: ExchangeService,
     private userService: UserService,
   ) {
     this.ownerWallet = this.configService.get<string>('ownerWallet');
-    this.provider = ethers.getDefaultProvider(
-      'https://rpc.ankr.com/bsc/5604b43661ba48f6ab7ef4b9970d5cd9b4fdb42357944ed24ca44374d640c604',
+    this.ownerNftWallet = this.configService.get<string>('ownerNftWallet');
+    this.nftAddress = this.configService.get<string>('nftAddress');
+    // this.providerAnkr = ethers.getDefaultProvider(
+    //   'https://rpc.ankr.com/bsc/5604b43661ba48f6ab7ef4b9970d5cd9b4fdb42357944ed24ca44374d640c604',
+    // );
+
+    // testnet
+    this.providerBsc = ethers.getDefaultProvider(
+      'https://data-seed-prebsc-1-s1.binance.org:8545',
     );
     this.savePresave();
   }
   async savePresave() {
     const contract = new Contract(
       '0x55d398326f99059fF775485246999027B3197955',
-      this.abi,
-      this.provider,
+      this.abiTransferEvent,
+      this.providerAnkr,
     );
 
     contract.on('Transfer', (from, to: string, _amount, event) => {
@@ -57,6 +71,29 @@ export class BlockchainService {
           createTime: moment().format('YYYY-MM-DD HH:mm:ss'),
         };
         this.exchangeService.createExchange(createData);
+      }
+    });
+  }
+
+  async onMintNft() {
+    const contract = new Contract(
+      this.nftAddress,
+      this.abiNft,
+      this.providerAnkr,
+    );
+
+    // because Ankr provider limit call
+    const contractBsc = new Contract(
+      this.nftAddress,
+      this.abiNft,
+      this.providerBsc,
+    );
+    contract.on('Transfer', (from, to: string, tokenId, event) => {
+      if (to == this.ownerNftWallet) {
+        contractBsc.getFunction('getTokenLevel')(tokenId).then((level) => {
+          // save new nft
+          // /nft/{level}/{tokenId}.json
+        });
       }
     });
   }
