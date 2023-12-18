5ea025aa835c78c9875e7c9190d6706769db49a8
Lu Nguyen <minhlu.nguyen@grevo.net>
2023-12-18 21:50:51 +0700

    Fix detail store nft

--- src/common/dtos/common.dto.ts
+++ src/common/dtos/common.dto.ts
@@ -1,4 +1,4 @@
-import { IsString, IsNotEmpty, Length } from 'class-validator';
+import { IsString, IsNotEmpty, Length, IsOptional } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 
 export class CommonDto {
@@ -7,6 +7,19 @@ export class CommonDto {
   @Length(42, 42)
   @ApiProperty({
     required: true,
+    description: 'Wallet address',
+    example: '0x264D6BF791f6Be6F001A95e895AE0a904732d473',
+    minLength: 42,
+    maxLength: 42,
+  })
+  wallet: string;
+}
+
+export class CommonOptionalDto {
+  @IsOptional()
+  @IsString()
+  @Length(42, 42)
+  @ApiProperty({
     description: 'Wallet address',
     example: '0x264D6BF791f6Be6F001A95e895AE0a904732d473',
     minLength: 42,
